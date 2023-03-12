bc617448d3bffa6bd10982cb9bbe7ed7a612c3a3
kirubasankar kirubasankar-chennai <kircyclone@gmail.com>
2023-03-12 14:34:22 +0530

    removing projects from base_url config

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['base_url'] = "http://".$_SERVER['HTTP_HOST']."/cicms/";
 
-/*
+/* 
 |--------------------------------------------------------------------------
 | Index File
 |--------------------------------------------------------------------------
